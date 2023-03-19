f5c3d635cde3ca7abe5f874ccfd5ffdfdfce5425
xuyuefeng <xuyf@shanhs.com>
2023-03-19 21:16:28 +0800

    feat: 别名配置

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,9 @@ module.exports = {
       {
         root: ['./src'],
         extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-        alias: {},
+        alias: {
+          '@': './src',
+        },
       },
     ],
   ],
