9f78ef93a541e33cf47685d34fea421ed2d237dc
xuyuefeng <xuyf@shanhs.com>
2023-03-20 15:45:19 +0800

    fix: webview编译问题

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,8 @@
 import React from 'react'
-import { SafeAreaView, StatusBar, useColorScheme } from 'react-native'
-
-import { Colors } from 'react-native/Libraries/NewAppScreen'
 import EasyWebView from '@/views/EasyWebView'
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark'
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  }
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <EasyWebView url='https://www.baidu.com' />
-    </SafeAreaView>
-  )
+  return <EasyWebView url='http://zd.8081.top' />
 }
 
 export default App
