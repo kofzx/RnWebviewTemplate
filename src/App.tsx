d90cb6afa5cbf02f04c80e0d7db94ca6179fcb47
xuyuefeng <xuyf@shanhs.com>
2023-03-25 07:45:26 +0800

    chore: app设置中文名跑模拟器会闪退。。。

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import EasyWebView from '@/views/EasyWebView'
 
 function App(): JSX.Element {
-  return <EasyWebView url='http://zd.8081.top' />
+  return <EasyWebView url='http://zd.8180.top' />
 }
 
 export default App
