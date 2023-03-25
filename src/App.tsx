63945135323d0ef7d86ec6d16c032553e403d72b
xuyuefeng <xuyf@shanhs.com>
2023-03-25 08:16:10 +0800

    feat: 添加启动页

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import EasyWebView from '@/views/EasyWebView'
+import SplashScreen from 'react-native-splash-screen'
 
 function App(): JSX.Element {
+  useEffect(() => {
+    setTimeout(() => {
+      console.log(111)
+      SplashScreen.hide() // 隐藏启动屏
+    }, 1000)
+  }, [])
+
   return <EasyWebView url='http://zd.8180.top' />
 }
 
